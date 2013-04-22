82223a19e540429297b6ea8f239b7f2e80af1049
Kurtis <americannimrod@gmail.com>
2013-04-21 17:03:44 -0700

    Initial Commit

---
+++ Transit App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Transit App
+//
+//  Created by Kurtis Schlepp on 4/21/13.
+//  Copyright (c) 2013 Kurtis Schlepp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Transit App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Transit App
+//
+//  Created by Kurtis Schlepp on 4/21/13.
+//  Copyright (c) 2013 Kurtis Schlepp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
